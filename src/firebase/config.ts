b71e5f8c8a30da40c8d2547b4739bd8e9526f101
Contato Repcom Ai <suporte@repcomai.com>
2026-02-25 18:23:12 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6708713514-3dc0e",
+  "appId": "1:741080420006:web:aebbe24c85a7f80b266e67",
+  "apiKey": "AIzaSyDt8TY9xASMbzFQXp1gpcXCY6C00lSr3N0",
+  "authDomain": "studio-6708713514-3dc0e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "741080420006"
+};
